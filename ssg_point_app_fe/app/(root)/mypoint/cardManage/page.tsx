c11fac0c2090f7b5d85e506dcb90516da127bf7c
daeunk17 <daeunk71@gmail.com>
2023-09-12 17:32:03 +0900

    [Fix]coupon reload error

--- ssg_point_app_fe/app/(root)/mypoint/cardManage/page.tsx
+++ ssg_point_app_fe/app/(root)/mypoint/cardManage/page.tsx
@@ -2,6 +2,7 @@ import CardManage from '@/components/page/mypoint/cardManage/CardManage'
 import React from 'react'
 
 export default function page() {
+  
   return (
     <CardManage/>
   )
--- ssg_point_app_fe/app/(root)/mypoint/pntHistory/page.tsx
+++ ssg_point_app_fe/app/(root)/mypoint/pntHistory/page.tsx
@@ -1,13 +1,12 @@
 
-
+"use client"
 import CardBox from '@/components/page/mypoint/CardBox'
 import PointList from '@/components/page/mypoint/PointList'
+import { useSession } from 'next-auth/react'
 import React from 'react'
 
 
 export default async function page() {
-
-  
 
   return (
     <div className='p-24'>
